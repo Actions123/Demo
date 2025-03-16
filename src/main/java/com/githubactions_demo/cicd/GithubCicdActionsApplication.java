89a84cc0fa2595f31ccfd928749471be3f65b41a
Sana <sanataba1995@gmail.com>
2025-03-16 16:39:39 +0530

    Update GithubCicdActionsApplication.java

--- src/main/java/com/githubactions_demo/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/githubactions_demo/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome";
+		return "Welcome to Github actions";
 	}
 
 //	echo "# github-actions-example" >> README.md
